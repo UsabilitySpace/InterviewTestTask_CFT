b4a256a94fcc727f8fac731651689dbe511aaafe
Usability.Space <info@usability.space>
2023-02-16 21:10:25 +0700

    2023.02.16 21:04:38

--- ЗАДАНИЕ_ГЛУШЕНКО_С_Н/ЗАДАНИЕ_1__SQL_запросы/ЗАДАНИЕ_1_query_003.sql
+++ ЗАДАНИЕ_ГЛУШЕНКО_С_Н/ЗАДАНИЕ_1__SQL_запросы/ЗАДАНИЕ_1_query_003.sql
@@ -9,6 +9,13 @@
 
 -- N.B.: IN ORACLE, "AS" IS ILLEGAL FOR MAKING TABLE ALIASES
 
+-- --------------
+-- ВАРИАНТ В.1:
+-- --------------
+-- В этом варианте выберутся только филиалы,
+-- в которых есть сотрудники
+-- --------------
+
 SELECT
 	t1.Department_Name  AS "Филиал",
 	t2.Department_Count AS "Число сотрудников"
@@ -16,6 +23,35 @@ FROM
 	DEPARTMENTS
 	t1
 RIGHT JOIN
+    (
+        SELECT
+            Department_ID,
+            count(*) AS Department_Count,
+        FROM
+            PERSONS
+        GROUP BY
+            Department_ID
+    )
+    t2
+ON
+	t1.Department_ID = t2.Department_ID
+;
+
+-- --------------
+-- ВАРИАНТ В.2:
+-- --------------
+-- Выберутся даже филиалы, в которых нет сотрудников,
+-- но для таких филиалов вместо числа сотрудников
+-- будет стоять NULL
+-- --------------
+
+SELECT
+	t1.Department_Name  AS "Филиал",
+	t2.Department_Count AS "Число сотрудников"
+FROM
+	DEPARTMENTS
+	t1
+LEFT JOIN
     (
         SELECT
             Department_ID,
